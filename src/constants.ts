a15f161eb08c8d404463c01a11daa2ac7d33c010
Hoang Vu <hoang.vu2@sotatek.com>
2023-04-10 17:06:19 +0700

    Init source

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0xCae66ac135d6489BDF5619Ae8F8f1e724765eb8f'
+
+export const INIT_CODE_HASH = '0x67a565926ef15dd0d941568bdb86fb9a06ce6e6053ad61f88aa88df72a0531ea'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
